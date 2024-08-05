6ff62b65650cb458dda1b4a66d77f05fc566266c
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-05 18:45:05 +0530

    Arithmetic Operators

---
+++ app/src/main/java/com/example/kotlinpracticals/Arithmatic_Operators.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinpracticals
+
+fun main() {
+    var a = 10
+    var b = 20
+
+        println("Addition of $a and $b is : ${a + b}")
+        println("Subtraction of $a and $b is : ${a - b}")
+        println("Multiplication of $a and $b is : ${a * b}")
+                println("Divison of $a and $b is : ${a / b}")
+}
