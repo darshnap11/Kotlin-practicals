9dae670c2d6458aa877b4b6f2db4ade45edba0a0
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-09 11:55:31 +0530

    Abstract

---
+++ app/src/main/java/com/example/kotlinpracticals/Abstract.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinpracticals
+
+
+abstract class Car{
+
+     abstract fun run()
+}
+
+class Honda: Car() {
+
+    override fun run() {
+        println("Honda is running.")
+    }
+}
+
+fun main(){
+    val obj = Honda()
+    obj.run()
+}
+
