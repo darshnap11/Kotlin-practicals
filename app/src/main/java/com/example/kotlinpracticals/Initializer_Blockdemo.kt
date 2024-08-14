8c7cb34c9497e535dd28aacf8cff5f52df0da6bc
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-14 12:10:33 +0530

    Initializer Block.

---
+++ app/src/main/java/com/example/kotlinpracticals/Initializer_Blockdemo.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinpracticals
+
+fun main(){
+    var person1 = Person("Darshna",21)
+
+}
+class Person(fName : String, personAge: Int){
+    val firstname = fName.capitalize()
+    var age = personAge
+    
+    init {
+        println("First Name = $firstname")
+        println("Age = $age")
+    }
+}
