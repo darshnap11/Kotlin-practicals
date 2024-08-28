83b050d9a5ed1c7cfaa31d660355beb27cf210d0
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-28 18:43:59 +0530

    FibonacciSeriesDemo.kt

---
+++ app/src/main/java/com/example/kotlinpracticals/FibonacciSeriesDemo.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinpracticals
+
+fun main() {
+    var number = 5
+    var a = 0
+    var b = 1
+
+    for(i in 1..number){
+        print("$a")
+        val sum = a + b
+        a = b
+        b = sum
+
+    }
+}
