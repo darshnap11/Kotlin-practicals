8624784914431cf39615d707998aa4589b4993f3
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-29 11:37:14 +0530

    PrimeNumberDemo.kt

---
+++ app/src/main/java/com/example/kotlinpracticals/PrimeNumberDemo.kt
@@ -0,0 +1,18 @@
+package com.example.kotlinpracticals
+
+fun main() {
+    val number = 11
+    var result = false
+
+    for (i in 2..number / 2){
+        if (number % i == 0){
+            result = true
+            break
+        }
+    }
+    if (!result)
+        println("$number is a prime number.")
+    else
+        println("$number is not a prime number.")
+
+}
