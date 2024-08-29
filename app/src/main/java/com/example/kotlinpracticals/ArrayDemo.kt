5a36f75c5d02bdc25b1bb921177b1313637db9ca
Darshna Pulpati <darshnapulpati19@gmail.com>
2024-08-29 13:45:12 +0530

    ArrayDemo.kt

--- app/src/main/java/com/example/kotlinpracticals/ArrayDemo.kt
+++ app/src/main/java/com/example/kotlinpracticals/ArrayDemo.kt
@@ -1,9 +1,18 @@
 package com.example.kotlinpracticals
 
 fun main() {
-    val numbers = arrayOf(10,20,30,40,50)
-    val sum = numbers.sum()
-    val average = sum.toDouble() / numbers.size
 
-    println("The average is : $average")
-}
+val array = intArrayOf(3,5,7,35,63,26)
+    val max = array.maxOrNull()
+
+    println("Largest Number is: $max")
+
+
+        val numbers = arrayOf(10, 20, 30, 40, 50)
+        val sum = numbers.sum()
+        val average = sum.toDouble() / numbers.size
+
+
+        println("The average is : $average")
+    }
+
